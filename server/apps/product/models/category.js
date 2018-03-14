765c04084c9da35d599326e7c2ac98c3e8994055
nickxin <nickxin@userdeMac-mini.local>
2018-03-14 10:31:55 +0800

    remove old version

--- server/apps/product/models/category.js
+++
@@ -1,9 +0,0 @@
-var restful = require('node-restful'),
-    mongoose = require('mongoose');
-	
-var Category = new restful.model(
-   'category',
-	mongoose.Schema({
-     name: 'string',
-  })).methods(['get', 'post', 'put', 'delete'])
-exports = module.exports = Category;
