ef184154ea7823d422969503424cbedd18093b73
Andy Sun <jsun@kenexa.com.cn>
2018-04-20 21:37:55 +0800

    update

--- src/dataModel/controller/parameter.js
+++ src/dataModel/controller/parameter.js
@@ -9,12 +9,28 @@ module.exports = class extends think.cloveradmin.rest {
         const ParameterModel = this.mongoose("parameter")
         var role_id = this.id
         var group = this.post("group")
+		var keepValue = this.post("keepValue")
+		console.log("keepValue=>",keepValue)
         const uid = await this.session("uid")
         var parameters = this.post("parameters")
-        await ParameterModel.remove({group:group})
+		if(keepValue)
+			await ParameterModel.remove({"group":group,"created":uid,"_id":{"$ne":keepValue}})
+		else
+			await ParameterModel.remove({"group":group,"created":uid})
         for(const parameterItem of parameters){
-            parameterItem.created = uid;
-            await ParameterModel.create(parameterItem)
+			if(parameterItem.value == keepValue && keepValue!=""){
+				//update
+				const info = await ParameterModel.findById(parameterItem.value)
+				if(info){
+					info.set(parameterItem)
+					await info.save()
+				}
+			}
+			else{
+				parameterItem.created = uid;
+				await ParameterModel.create(parameterItem)
+			}
+            
         }
         return this.success({})
     }
